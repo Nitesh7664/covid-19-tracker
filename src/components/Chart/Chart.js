39a9a47ccbec1f5e469b46bd0e61e658818f0ae9
Nitesh7664 <sharma.nitesh7664@gmail.com>
2020-05-19 19:57:30 +0530

    installed chart.js as dependency

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 
+import { Line, Bar } from "react-chartjs-2";
+
 function Chart() {
   const [dailyData, setDailyData] = useState([]);
 
@@ -12,6 +14,29 @@ function Chart() {
     console.log(dailyData);
     fetchData();
   }, [setDailyData]);
+
+  const LineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#22f",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "#f22",
+            backgroundColor: "rgba(2, 2, 255, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
 
   return (
     <div>
